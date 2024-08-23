18afc13bf48463a0c4ca5bb637db9e9ec5493346
Mystist <gjl87910lq@gmail.com>
2024-08-23 08:28:49 -0700

    chore: initial deploy

---
+++ terraform/main.tf
@@ -0,0 +1,529 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+data "aws_availability_zones" "available" {}
+
+# vpc
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "${var.project_name}-vpc"
+  }
+}
+
+resource "aws_subnet" "public" {
+  count                   = var.az_count
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${var.project_name}-public-subnet-${count.index + 1}"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.project_name}-igw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = var.az_count
+  subnet_id      = element(aws_subnet.public[*].id, count.index)
+  route_table_id = aws_route_table.public.id
+}
+
+# security group
+resource "aws_security_group" "nlb" {
+  name        = "${var.project_name}-nlb-sg"
+  description = "Security group for NLB"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-nlb-sg"
+  }
+}
+
+resource "aws_security_group" "ecs_tasks" {
+  name        = "${var.project_name}-ecs-tasks-sg"
+  description = "Allow inbound traffic to ECS tasks"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "Allow inbound HTTP traffic"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow inbound HTTPS traffic"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-ecs-tasks-sg"
+  }
+}
+
+resource "aws_security_group" "efs" {
+  name        = "allow_efs_access"
+  description = "Allow EFS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description     = "NFS from ECS tasks"
+    from_port       = 2049
+    to_port         = 2049
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ecs_tasks.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# nlb
+resource "aws_lb" "main" {
+  name               = "${var.project_name}-nlb"
+  internal           = false
+  load_balancer_type = "network"
+  subnets            = aws_subnet.public[*].id
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "${var.project_name}-nlb"
+  }
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "443"
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.https.arn
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.http.arn
+  }
+}
+
+resource "aws_lb_target_group" "https" {
+  name        = "${var.project_name}-tg-https"
+  port        = 443
+  protocol    = "TCP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "ip"
+
+  health_check {
+    enabled             = true
+    interval            = 30
+    port                = "traffic-port"
+    protocol            = "TCP"
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+  }
+
+  tags = {
+    Name = "${var.project_name}-tg-https"
+  }
+}
+
+resource "aws_lb_target_group" "http" {
+  name        = "${var.project_name}-tg-http"
+  port        = 80
+  protocol    = "TCP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "ip"
+
+  health_check {
+    enabled             = true
+    interval            = 30
+    port                = "traffic-port"
+    protocol            = "TCP"
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+  }
+
+  tags = {
+    Name = "${var.project_name}-tg-http"
+  }
+}
+
+# efs
+resource "aws_efs_file_system" "cert_storage" {
+  creation_token = "cert-storage"
+  encrypted      = true
+
+  tags = {
+    Name = "CertificateStorage"
+  }
+}
+
+resource "aws_efs_mount_target" "cert_storage" {
+  count           = length(aws_subnet.public)
+  file_system_id  = aws_efs_file_system.cert_storage.id
+  subnet_id       = aws_subnet.public[count.index].id
+  security_groups = [aws_security_group.efs.id]
+}
+
+# ecs
+resource "aws_ecs_cluster" "main" {
+  name = "${var.project_name}-cluster"
+}
+
+resource "aws_ecs_task_definition" "main" {
+  family                   = "${var.project_name}-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = var.fargate_cpu
+  memory                   = var.fargate_memory
+
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name  = "lb"
+      image = var.lb_image
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        },
+        {
+          containerPort = 443
+          hostPort      = 443
+        }
+      ]
+      mountPoints = [
+        {
+          sourceVolume  = "cert-storage"
+          containerPath = "/etc/certs"
+          readOnly      = true
+        }
+      ]
+      essential = true
+
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = "/ecs/${var.project_name}"
+          awslogs-region        = var.aws_region
+          awslogs-stream-prefix = "lb"
+        }
+      }
+    }
+  ])
+
+  volume {
+    name = "cert-storage"
+
+    efs_volume_configuration {
+      file_system_id = aws_efs_file_system.cert_storage.id
+      root_directory = "/"
+    }
+  }
+}
+
+resource "aws_ecs_service" "lb_service" {
+  name            = "lb-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.main.arn
+  desired_count   = var.app_count
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = aws_subnet.public[*].id
+    security_groups  = [aws_security_group.ecs_tasks.id]
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.https.arn
+    container_name   = "lb"
+    container_port   = 443
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.http.arn
+    container_name   = "lb"
+    container_port   = 80
+  }
+
+  service_registries {
+    registry_arn = aws_service_discovery_service.lb_service.arn
+  }
+
+  depends_on = [aws_lb_listener.https, aws_lb_listener.http]
+}
+
+resource "aws_ecs_service" "todo_service" {
+  name            = "todo-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.main.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = aws_subnet.public[*].id
+    security_groups  = [aws_security_group.ecs_tasks.id]
+  }
+
+  service_registries {
+    registry_arn = aws_service_discovery_service.todo_service.arn
+  }
+}
+
+# iam
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "${var.project_name}-ecs-task-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# cloudwatch
+resource "aws_cloudwatch_log_group" "ecs_logs" {
+  name              = "/ecs/${var.project_name}"
+  retention_in_days = 30
+}
+
+# cloud map
+resource "aws_service_discovery_private_dns_namespace" "main" {
+  name        = "application.private"
+  description = "Private namespace for service discovery"
+  vpc         = aws_vpc.main.id
+}
+
+resource "aws_service_discovery_service" "lb_service" {
+  name = "lb_service"
+
+  dns_config {
+    namespace_id = aws_service_discovery_private_dns_namespace.main.id
+
+    dns_records {
+      ttl  = 10
+      type = "A"
+    }
+  }
+
+  health_check_custom_config {
+    failure_threshold = 1
+  }
+}
+
+resource "aws_service_discovery_service" "todo_service" {
+  name = "todo_service"
+
+  dns_config {
+    namespace_id = aws_service_discovery_private_dns_namespace.main.id
+
+    dns_records {
+      ttl  = 10
+      type = "A"
+    }
+  }
+
+  health_check_custom_config {
+    failure_threshold = 1
+  }
+}
+
+# variables
+variable "aws_region" {
+  description = "The AWS region to create resources in"
+  default     = "ap-east-1"
+}
+
+variable "project_name" {
+  description = "Todo Application"
+  default     = "todo"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  default     = "172.71.0.0/16"
+}
+
+variable "az_count" {
+  description = "Number of AZs to cover in a given region"
+  default     = 2
+}
+
+variable "app_image" {
+  description = "Docker image to run in the ECS cluster"
+  default     = "todo:latest"
+}
+
+variable "lb_image" {
+  description = "Docker image for the load balancer container"
+  default     = "lb:latest"
+}
+
+variable "app_port" {
+  description = "Port exposed by the docker image to redirect traffic to"
+  default     = 3000
+}
+
+variable "app_count" {
+  description = "Number of docker containers to run"
+  default     = 1
+}
+
+variable "fargate_cpu" {
+  description = "Fargate instance CPU units to provision (1 vCPU = 1024 CPU units)"
+  default     = "256"
+}
+
+variable "fargate_memory" {
+  description = "Fargate instance memory to provision (in MiB)"
+  default     = "512"
+}
+
+variable "ecs_autoscale_min_instances" {
+  description = "Minimum autoscale instances"
+  default     = 1
+}
+
+variable "ecs_autoscale_max_instances" {
+  description = "Maximum autoscale instances"
+  default     = 4
+}
+
+variable "ecs_autoscale_cpu_target" {
+  description = "Target CPU utilization for autoscaling"
+  default     = 90
+}
+
+# output
+output "vpc_id" {
+  value       = aws_vpc.main.id
+  description = "ID of the VPC"
+}
+
+output "nlb_dns_name" {
+  value       = aws_lb.main.dns_name
+  description = "The DNS name of the NLB"
+}
+
+output "target_group_arns" {
+  value = {
+    http  = aws_lb_target_group.http.arn
+    https = aws_lb_target_group.https.arn
+  }
+  description = "ARNs of the target groups"
+}
+
+output "ecs_cluster_name" {
+  value       = aws_ecs_cluster.main.name
+  description = "Name of the ECS cluster"
+}
+
+output "ecs_task_definition" {
+  value       = aws_ecs_task_definition.main.arn
+  description = "ARN of the ECS task definition"
+}
+
+output "ecs_lb_service_name" {
+  value       = aws_ecs_service.lb_service.name
+  description = "Name of the ECS lb service"
+}
+
+output "ecs_todo_service_name" {
+  value       = aws_ecs_service.todo_service.name
+  description = "Name of the ECS todo service"
+}
